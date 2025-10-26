e86d6397f04d0eae0fcdb92cd2b2e66a7ddfbaee
Corvus-7 <volchkowa.d@gmail.com>
2025-10-26 17:03:36 +0300

    Update Python.py

--- Task_2/Python.py
+++ Task_2/Python.py
@@ -1,4 +1,3 @@
-> kk
 # Бинарная куча
 import heapq
 Numbers = [8, 3, 5, 1, 6, 2, 4, 7]  # создание списка с исходными данными для кучи
@@ -165,6 +164,7 @@ print(ht.get("Alice"))  # January
 ht.remove("Bob")
 
 print(ht.get("Bob"))    # KeyError: Bo
+
 
 
 
